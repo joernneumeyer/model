1554c084c4da229da91591ffbd93310f385d121c
Jörn Neumeyer <contact@joern-neumeyer.de>
2021-09-14 16:33:49 +0200

    add validation error propagation

--- src/Model/ModelParser.php
+++ src/Model/ModelParser.php
@@ -3,6 +3,7 @@
   namespace Neu\Model;
 
   use InvalidArgumentException;
+  use Neu\Model\Exceptions\ValidationError;
   use ReflectionClass;
   use ReflectionException;
   use ReflectionObject;
@@ -25,21 +26,40 @@
      * Serializes an object into an associative array.
      * @param object $obj
      * @return array<string, mixed>
+     * @throws ValidationError
      */
     public function serialize(object $obj): array {
       $data = [];
-      $ref = new ReflectionObject($obj);
+      $ref  = new ReflectionObject($obj);
+      $errors = [];
       foreach ($ref->getProperties() as $property) {
         $property->setAccessible(true);
         $group = new TransformGroup(property: $property, source: $obj, value: $property->getValue($obj), name: $property->getName());
         if ($property->getType() instanceof \ReflectionNamedType && class_exists($property->getType()->getName())) {
-          $group->value = self::serialize($group->value);
+          try {
+            $group->value = self::serialize($group->value);
+          } catch (ValidationError $e) {
+            foreach ($e->errors as $key => $error) {
+              $errors[$property->getName() . '.' . $key] = $error;
+            }
+          }
         }
         foreach (self::propertyModelAttributes($property) as $processor) {
-          $processor->serialize($group);
+          try {
+            $processor->serialize($group);
+          } catch (ValidationError $e) {
+            foreach ($e->errors as $error) {
+              $errors[$property->getName()][] = $error;
+            }
+          }
         }
-        if ($group->ignoreField) continue;
+        if ($group->ignoreField) {
+          continue;
+        }
         $data[$group->name] = $group->value;
+      }
+      if ($errors) {
+        throw new ValidationError($errors);
       }
       return $data;
     }
@@ -49,28 +69,48 @@
      * @param string $type
      * @return object
      * @throws ReflectionException
+     * @throws ValidationError
      */
     public function deserialize(array $data, string $type): object {
       if (!class_exists($type)) {
         throw new InvalidArgumentException('Cannot deserialize array to an object of undefined type "' . $type . '"!');
       }
-      $ref = new ReflectionClass($type);
+      $ref           = new ReflectionClass($type);
       $ctorArgsCount = $ref->getConstructor()?->getNumberOfRequiredParameters();
       if ($ctorArgsCount !== null && $ctorArgsCount > 0) {
         throw new InvalidArgumentException('Cannot deserialize data into an object of type "' . $type . '" as its constructor is non-trivial!');
       }
-      $obj = $ref->newInstance();
+      $obj    = $ref->newInstance();
+      $errors = [];
       foreach ($ref->getProperties() as $property) {
         $property->setAccessible(true);
         $group = new TransformGroup(property: $property, source: $obj, value: $data[$property->getName()] ?? null, name: $property->getName(), modelData: $data);
         if ($property->getType() instanceof \ReflectionNamedType && class_exists($property->getType()->getName())) {
-          $group->value = $this->deserialize($group->value, $property->getType()->getName());
+          try {
+            $group->value = $this->deserialize($group->value, $property->getType()->getName());
+          } catch (ValidationError $e) {
+            foreach ($e->errors as $key => $error) {
+              $errors[$property->getName() . '.' . $key] = $error;
+            }
+            continue;
+          }
         }
         foreach (self::propertyModelAttributes($property) as $processor) {
-          $processor->deserialize($group);
+          try {
+            $processor->deserialize($group);
+          } catch (ValidationError $e) {
+            foreach ($e->errors as $error) {
+              $errors[$property->getName()][] = $error;
+            }
+          }
         }
-        if ($group->ignoreField) continue;
+        if ($group->ignoreField) {
+          continue;
+        }
         $obj->{$group->name} = $group->value;
+      }
+      if ($errors) {
+        throw new ValidationError($errors);
       }
       return $obj;
     }
